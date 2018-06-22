afaf211b4e633a5d46cc20d0239790cbb5618038
Andrew Wrigley <andrew@wrigley.io>
2018-06-22 13:54:39 +0800

    Add code to plot data from files

--- libdai/src/tbp.cpp
+++ libdai/src/tbp.cpp
@@ -20,7 +20,9 @@ namespace dai {
         PropertySet opts;
         opts.set("verbose",(size_t)0);       // Verbosity (amount of output generated)
 
-        string heuristic = "RANDOM";  // or MINFILL
+//        string heuristic = "RANDOM";
+        string heuristic = "MINFILL";
+        cerr << "Using elimintation heuristic: " << heuristic << endl;
         JTree jt(fg, opts("updates",string("SHSH"))("heuristic", heuristic));
 
         jt.init();
